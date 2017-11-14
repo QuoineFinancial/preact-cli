6e328a4d97a54c0845d385e324e5a4c2535aefb8
Huong Hk <huong.quoine@gmail.com>
2017-11-14 16:34:29 +0700

    Polyfill Headers

--- src/lib/webpack/polyfills.js
+++ src/lib/webpack/polyfills.js
@@ -1,2 +1,3 @@
 if (!global.Promise) global.Promise = require('promise-polyfill');
 if (!global.fetch) global.fetch = require('isomorphic-unfetch');
+if (!global.Headers) global.Headers = require('fetch-headers');
